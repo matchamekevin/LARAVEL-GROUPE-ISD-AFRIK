06a63cb35d666a68a486fcec9f9f7c940e81e7f7
matchamekevin <matchamegnatikevin894@gmail.com>
2026-03-26 13:53:18 +0000

    fix 17

--- resources/js/axios.js
+++ resources/js/axios.js
@@ -11,7 +11,7 @@ const computedBase = apiBase ? `${apiBase.replace(/\/$/, '')}/api` : '/api';
 
 const api = axios.create({
   baseURL: computedBase,
-  timeout: 15000, // 15s timeout to avoid hanging requests in prod
+  timeout: 30000, // 30s timeout in case of slow prod responses
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
@@ -39,6 +39,18 @@ api.interceptors.response.use(
     } catch (e) {
       // ignore logging errors
     }
+
+    // simple retry/backoff for network timeouts or missing response
+    const config = err.config || {};
+    if (!config) return Promise.reject(err);
+    config.__retryCount = config.__retryCount || 0;
+    const shouldRetry = (err.code === 'ECONNABORTED' || !err.response) && config.__retryCount < 2;
+    if (shouldRetry) {
+      config.__retryCount += 1;
+      const delay = 500 * config.__retryCount; // 500ms, 1s
+      return new Promise((resolve) => setTimeout(resolve, delay)).then(() => api(config));
+    }
+
     return Promise.reject(err);
   }
 );
